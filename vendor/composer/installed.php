495603be00ad3f47812c9449b75954c4de7204bd
E-pic-C <etsawyer37@gmail.com>
2024-09-04 05:28:32 +0000

    Eric_Todd_Sawyer "<god>" Privacy_Poliacy Epic_License_Aggrement

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'fortawesome/font-awesome',
+        'pretty_version' => 'dev-patch-1',
+        'version' => 'dev-patch-1',
+        'reference' => '01886907e64998efee36b79abeb4e2954b7d80f3',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'fortawesome/font-awesome' => array(
+            'pretty_version' => 'dev-patch-1',
+            'version' => 'dev-patch-1',
+            'reference' => '01886907e64998efee36b79abeb4e2954b7d80f3',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
